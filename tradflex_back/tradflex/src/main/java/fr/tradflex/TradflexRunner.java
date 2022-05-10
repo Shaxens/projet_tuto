0568a7fef1d86f1deebfc8348906102aeaeea048
Shaxens <arthurvietti@gmail.com>
2022-05-10 11:49:20 +0200

    Eh bah ça marche pas

--- tradflex_back/tradflex/src/main/java/fr/tradflex/TradflexRunner.java
+++ tradflex_back/tradflex/src/main/java/fr/tradflex/TradflexRunner.java
@@ -1,20 +1,20 @@
 package fr.tradflex;
 
+import fr.tradflex.projet.*;
 import fr.tradflex.projet.Projet;
-import fr.tradflex.projet.Film;
-import fr.tradflex.utilisateur.Utilisateur;
+import fr.tradflex.utilisateur.*;
 
+import java.util.List;
 import java.util.ArrayList;
 
 
-public class TradflexRunner
-{
+public class TradflexRunner {
     public static void main( String[] args ) throws Exception {
 
         Utilisateur utilisateur1 = new Utilisateur(1, "Goldorak");
         utilisateur1.toString();
 
-        ArrayList<Utilisateur> groupeUtilisateur = new ArrayList<> ();
+        List<Utilisateur> groupeUtilisateur = new ArrayList<> ();
         groupeUtilisateur.add(utilisateur1);
         Film film1 = new Film(1, "Avatar", 180, 2498452);
 
@@ -23,11 +23,9 @@ public class TradflexRunner
 
 
         Film film2 = new Film(2, "Matrix", 163, 986523);
-        projet1.ajouterFilm(film1);
 
         film1.toString();
 
-        projet1.ajouterFilm(film2);
         film2.toString();
     }
 }
