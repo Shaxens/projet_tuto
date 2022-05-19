c0611938e65d089c840715e23917763d963ce8cb
DrYruo <oury.jordan@hotmail.com>
2022-05-19 21:06:59 +0200

    MAJ on reprends du debut

---
+++ tradflex_back/tradflex/src/main/java/fr/tradflex/model/Project.java
@@ -0,0 +1,5 @@
+package fr.tradflex.model;
+
+import java.io.Serializable;
+
+public record Project(String id, String nom) implements Serializable {}
--- tradflex_back/tradflex/src/main/java/fr/tradflex/utilisateur/Contact.java
+++
@@ -1,7 +0,0 @@
-package fr.tradflex.utilisateur;
-
-public record Contact(int id, String nom, String prenom, String mail) {
-
-    // Méthodes
-
-}
