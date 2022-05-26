b3426bbc1702f81017b4240bab4e642e7f67c326
Farronilucas <farronilucas@hotmail.fr>
2022-05-26 21:19:53 +0200

    conflitResolus

--- tradflex_front/tradflex-app/src/app/app-routing.module.ts
+++ tradflex_front/tradflex-app/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormulaireFilmComponent } from './formulaire-film/formulaire-film.component';
+import { FormulaireFilmComponent } from './DirectoryFilm/formulaire-film/formulaire-film.component';
 import { MainPageComponent } from './main-page/main-page.component';
 
 const routes: Routes = [
@@ -11,7 +11,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  "imports": [RouterModule.forRoot(routes)],
+  "exports": [RouterModule]
 })
 export class AppRoutingModule { }
