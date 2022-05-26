df3cf7d166f86875d07c070d747abb948eada924
Farronilucas <farronilucas@hotmail.fr>
2022-05-26 15:45:55 +0200

    test

---
+++ tradflex_front/tradflex-app/src/app/formulaire-film/formulaire-film.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Film } from '../film';
+import { FILM } from '../filmList';
+@Component({
+  selector: 'app-formulaire-film',
+  templateUrl: 'formulaire-film.component.html',
+  styleUrls: ['formulaire-film.component.css']
+})
+export class FormulaireFilmComponent implements OnInit {
+film = new Film(3,"jambon");
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onSubmit(){
+  console.log('Submit form');
+
+  }
+}
