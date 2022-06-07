d296a7c91906755fea1bc1eaba8e829963037624
Shaxens <arthurvietti@gmail.com>
2022-06-07 18:21:25 +0200

    Ajout des timer globaux de la vidéo

--- tradflex_front/tradflex-app/src/app/editer-projet/editer-projet.component.ts
+++ tradflex_front/tradflex-app/src/app/editer-projet/editer-projet.component.ts
@@ -1,6 +1,5 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import WaveSurfer from 'wavesurfer.js';
-import MediaElement from 'wavesurfer.js/src/mediaelement';
 import TimelinePlugin from 'wavesurfer.js/src/plugin/timeline';
 
 @Component({
@@ -9,17 +8,22 @@ import TimelinePlugin from 'wavesurfer.js/src/plugin/timeline';
   styleUrls: ['./editer-projet.component.css']
 })
 export class EditerProjetComponent implements OnInit {
-
+  
+  timeTotal!: HTMLElement;
+  currentTime!: HTMLElement;
+  timeRemaining!: HTMLElement;
+  
   video!: WaveSurfer;
-  // url = "../assets/video/PETIT_BISCUIT-Sunset_Lover.mp4";
+  url = "../assets/video/PETIT_BISCUIT-Sunset_Lover.mp4";
+
   // url = "../assets/video/Parodie_300.mp4";
-  url = "../assets/video/Corneil_et_bernie.mp4";
+  // url = "../assets/video/Corneil_et_bernie.mp4";
   // url = "https://ia800508.us.archive.org/15/items/LoveThemeFromTheGodfather/02LoveThemeFromTheGodfather.mp3";
 
   constructor( private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
-  }
+}
 
   generateVideo(): void {
     Promise.resolve(null).then(() => {
@@ -39,6 +43,25 @@ export class EditerProjetComponent implements OnInit {
       });
       this.video.on('ready', () => {
         this.video.play();
+        this.video.getDuration();
+      })
+      this.video.on('audioprocess', () => {
+        if (this.video.isPlaying()) {
+          var totalTime = this.video.getDuration(),
+          currentTime = this.video.getCurrentTime(),
+          timeRemaining = totalTime - currentTime;
+
+          this.timeTotal = document.getElementById('time-total') as HTMLElement;
+          this.timeTotal.innerText = totalTime.toFixed(1);
+
+          this.currentTime = document.getElementById('time-current') as HTMLElement;
+          this.currentTime.innerText = currentTime.toFixed(1);
+
+          this.timeRemaining = document.getElementById('time-remaining') as HTMLElement;
+          this.timeRemaining.innerText = timeRemaining.toFixed(1);
+        }
+        
+
       })
     })
   }
