17808a164ed588c4dfc7414c9f277e6465c0f1cc
Farronilucas <farronilucas@hotmail.fr>
2022-06-16 15:47:27 +0200

    fetch projet

--- tradflex_front/tradflex-app/src/app/main-page/main-page.component.ts
+++ tradflex_front/tradflex-app/src/app/main-page/main-page.component.ts
@@ -10,41 +10,49 @@ import { ProjetService } from '../DirectoryFilm/projet.service';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
-  projetList: Projet[] = [] ;
+  projetList: Projet[] = [];
 
-  projet: Projet|undefined;
+  projet: Projet | undefined;
+
   constructor(private router: Router,
-  private projetService : ProjetService) { }
+              private projetService: ProjetService) {
+  }
 
   ngOnInit(): void {
+    /*
+        this.projetService.getProjetList()
+          .subscribe(projetList => projetList.forEach(projectId => {
+            this.projetService.getProjetById(projectId.id)
+              .subscribe(projet => {
+                if (projet != undefined) {
+                  this.projetList.push(projet)
+                }
+              })
+          })); //je m'abonne au flux de donnée de l'observable pour recevoir une liste de film
+        //quand je reçois la liste de film je la pousse dans filmList
+        this.projetList?.forEach(projet => this.projetService.getProjetById(projet.id))
+      }
 
-    this.projetService.getProjetList()
-      .subscribe(projetList => projetList.forEach(projectId => {
-        this.projetService.getProjetById(projectId.id)
-          .subscribe(projet => {
-            if (projet != undefined) {
-              this.projetList.push(projet)
-            }
-          })
-      })); //je m'abonne au flux de donnée de l'observable pour recevoir une liste de film
-    //quand je reçois la liste de film je la pousse dans filmList
-    this.projetList?.forEach(projet => this.projetService.getProjetById(projet.id))
+      creerProjet() {
+        this.router.navigate(['creerProjet']);
+      }
+
+      editerProjet(projet: Projet) {
+        this.router.navigate(['editer', projet.id]);
+      }
+
+      modifierProjet(projet : Projet) {
+        this.router.navigate(['parametre', projet.id]);
+        }
+
+      SupprimerProjet(projet :Projet){
+        this.projetService.deleteProjetById(projet.id).subscribe( );
+      }
+    */
+    fetch(`http://localhost:8080/project/`).then(res => res.json())
+      .then(jsonData => {
+        console.log(jsonData);
+        return this.projetList = jsonData;
+      });
   }
-
-  creerProjet() {
-    this.router.navigate(['creerProjet']);
-  }
-
-  editerProjet(projet: Projet) {
-    this.router.navigate(['editer', projet.id]);
-  }
-
-  modifierProjet(projet : Projet) {
-    this.router.navigate(['parametre', projet.id]);
-    }
-
-  SupprimerProjet(projet :Projet){
-    this.projetService.deleteProjetById(projet.id).subscribe( );
-  }
-
 }
